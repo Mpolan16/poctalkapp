c4a7142257cc759b006f9f9da4722eb22870e8be
Laura <lhernan53112@gmail.com>
2020-11-08 16:45:26 -0500

    Added button to home page

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Searchbar from '../../components/Searchbar';
+import Button from '../../components/Button';
 
 function Home() {
     return (
         <>
         <Searchbar/>
+        <Button/>
         </>
     );
 }
