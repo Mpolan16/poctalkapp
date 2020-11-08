c55cb5babc749fc837a778ca5b01ae107e67d9a1
Laura <lhernan53112@gmail.com>
2020-11-08 16:23:38 -0500

    Added home page with searchbar

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Searchbar from '../../components/Searchbar';
+
+function Home() {
+    return (
+        <>
+        <Searchbar/>
+        </>
+    );
+}
+
+export default Home;
