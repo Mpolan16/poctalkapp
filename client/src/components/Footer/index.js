7de45e4bfbc49f7fd6cdf235a57d6d17ca7a991c
Laura <lhernan53112@gmail.com>
2020-11-08 15:58:43 -0500

    created footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+    <footer className="footer">
+        <div className="col-md-12" >
+            <p >Las Super Poderosas </p>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
