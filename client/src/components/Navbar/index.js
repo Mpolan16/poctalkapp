fa41603e97f76a11ac776c66dd3802c86f2f97ae
Laura <lhernan53112@gmail.com>
2020-11-09 17:59:08 -0500

    Added contact page

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -11,6 +11,7 @@ function NavBar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
