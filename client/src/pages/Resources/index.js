991bf60cfcc677beb2b43c854d138d3310b2e1c0
Laura <lhernan53112@gmail.com>
2020-11-10 13:23:11 -0500

    Added Resources page

---
+++ client/src/pages/Resources/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Searchbar from '../../components/Searchbar';
+import Button from '../../components/Button';
+
+function Resource() {
+    return (
+        <>
+       <Searchbar/>
+        <Button/>
+        </>
+    );
+}
+
+export default Resource;
